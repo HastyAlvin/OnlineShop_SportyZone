2c15fab31a4d5d145dc1e55aa0102c7974aacfc0
tuandthe161447 <tuandthe161447@fpt.edu.vn>
2025-02-04 23:09:43 +0700

    build database

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,42 +1,45 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
 const userSchema = new Schema(
-    {
-      username: {
-        type: String,
-        required: [true, "Tên người dùng không được để trống"],
-      },
-      email: {
-        type: String,
-        required: [true, "Email không được để trống"],
-      },
-      password: {
-        type: String,
-        required: [true, "Mật khẩu không được để trống"],
-        min: [6, "Mật khẩu phải dài hơn 6 chữ số"],
-      },
-      role: {
-        type: Number,
-        required: [true, "Role không được để trống"],
-      },
-      groupId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: Group,
-      },
-      semesterId: [{ type: mongoose.Schema.Types.ObjectId, ref: "Semester" }],
-      isLeader: { type: Boolean, default: "false" },
-      status: {
-        type: String,
-        enum: ["Active", "InActive", "Disabled", "Pending"],
-        default: "InActive",
-      },
-      rollNumber: { type: String },
-      memberCode: { type: String },
-      major: { type: String },
+  {
+    username: {
+      type: String,
+      required: [true, "Tên người dùng không được để trống"],
     },
-    {
-      timestamps: true,
-    }
-  );
-  
-  const User = mongoose.model("User", userSchema);
-  module.exports = User;
+    email: {
+      type: String,
+      required: [true, "Email không được để trống"],
+    },
+    password: {
+      type: String,
+      required: [true, "Mật khẩu không được để trống"],
+      min: [6, "Mật khẩu phải dài hơn 6 chữ số"],
+    },
+    role: {
+      type: String,
+      required: [true, "Role không được để trống"],
+    },
+    full_name: {
+      type: String,
+      required: [true, "Tên đầy đủ không được để trống"],
+    },
+    phone_number: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    isActive: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,17 @@ const bodyParser = require('body-parser');
 const httpErrors = require('http-errors');
 require('dotenv').config();
 
+// Gọi router
+const userRoutes = require('./routers/userRouter.js');
+
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+
+// API
+app.use('/api', userRoutes); 
 
 app.get('/', async (req, res, next) => {
     res.status(200).json({ "message": "Welcome to RESTFul API Server" });
